2beb51d498d18fc72e958da969ba0aac3872e011
faucetd <papasfreezeriatime@gmail.com>
2023-01-05 00:36:14 -0500

    favicon added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6f550d14a99e268d"; }
+/******/ 		__webpack_require__.h = function() { return "a16631a41cfa1874"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
