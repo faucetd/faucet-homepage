f310580d302323175c5c03c4c89f58d3def50b39
faucetd <107884235+faucetd@users.noreply.github.com>
2023-02-11 22:01:36 -0500

    next.js dependencies

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "adab30a118011ca0"; }
+/******/ 		__webpack_require__.h = function() { return "8e27fc11a1950183"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
