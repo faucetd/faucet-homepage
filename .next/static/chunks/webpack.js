a099acd1f380bc0c2cbb53e3a2f7591b900e81d2
faucetd <107884235+faucetd@users.noreply.github.com>
2023-02-17 13:08:38 -0500

    messing with favicon

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bd1a287a694e83e7"; }
+/******/ 		__webpack_require__.h = function() { return "ed6c9e61e2edd7f0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/layouts/main.js
+++ components/layouts/main.js
@@ -8,8 +8,11 @@ const Main = ({ children, router }) => {
         <Box as="main" pb={8}>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <link rel="shortcut icon" href="/images/favicon.ico" />
+                <meta name="description" content="Faucet's homepage" />
+                <meta name="author" content="faucet" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+                <link rel="shortcut icon" href="/images/images/favicon.ico" />
+                
                 <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
                 <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
                 <title>faucet - homepage</title>
