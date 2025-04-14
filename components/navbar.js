f9a86aba93ce20fb9f969fcc357442b809c1aa0b
rebeccacombs <rebecca.brushes@gmail.com>
2025-04-14 13:18:17 +0800

    pushing so i dont have to save

--- components/navbar.js
+++ components/navbar.js
@@ -1,3 +1,4 @@
+import { forwardRef } from 'react'
 import Logo from './logo'
 import NextLink from 'next/link'
 import {
@@ -20,21 +21,26 @@ import { IoLogoGithub } from 'react-icons/io5'
 
 const LinkItem = ({ href, path, target, children, ...props }) => {
   const active = path === href
-  const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
+  const inactiveColor = useColorModeValue('gray.800', 'whiteAlpha.900')
   return (
-    <NextLink href={href} passHref scroll={false}>
-      <Link
-        p={2}
-        bg={active ? 'grassTeal' : undefined}
-        color={active ? '#202023' : inactiveColor}
-        target={target}
-        {...props}
-      >
-        {children}
-      </Link>
-    </NextLink>
+    <Link
+      as={NextLink}
+      href={href}
+      scroll={false}
+      p={2}
+      bg={active ? 'grassTeal' : undefined}
+      color={active ? '#202023' : inactiveColor}
+      target={target}
+      {...props}
+    >
+      {children}
+    </Link>
   )
 }
+
+const MenuLink = forwardRef((props, ref) => (
+  <Link ref={ref} as={NextLink} {...props} />
+))
 
 const Navbar = props => {
   const { path } = props
@@ -52,13 +58,13 @@ const Navbar = props => {
       <Container
         display="flex"
         p={2}
-        maxW="container.xl"
+        maxW="container.md"
         wrap="wrap"
         align="center"
         justify="space-between"
       >
         <Flex align="center" mr={5}>
-          <Heading as="h1" size="xs" letterSpacing={'tighter'}>
+          <Heading as="h1" size="lg" letterSpacing={'tighter'}>
             <Logo />
           </Heading>
         </Flex>
@@ -74,13 +80,16 @@ const Navbar = props => {
           <LinkItem href="/works" path={path}>
             Works
           </LinkItem>
-          <LinkItem href="/blog" path={path}>
-            Blog
+          <LinkItem href="https://store.craftz.dog/" path={path}>
+            Wallpapers
           </LinkItem>
-          
+          <LinkItem href="/posts" path={path}>
+            Posts
+          </LinkItem>
+          <LinkItem href="https://uses.craftz.dog/">Uses</LinkItem>
           <LinkItem
             target="_blank"
-            href="https://github.com/faucetd/faucet-homepage"
+            href="https://github.com/craftzdog/craftzdog-homepage"
             path={path}
             display="inline-flex"
             alignItems="center"
@@ -104,19 +113,24 @@ const Navbar = props => {
                 aria-label="Options"
               />
               <MenuList>
-                <NextLink href="/" passHref>
-                  <MenuItem as={Link}>About</MenuItem>
-                </NextLink>
-                <NextLink href="/works" passHref>
-                  <MenuItem as={Link}>Works</MenuItem>
-                </NextLink>
-                <NextLink href="/blog" passHref>
-                  <MenuItem as={Link}>Blog</MenuItem>
-                </NextLink>
-                
+                <MenuItem as={MenuLink} href="/">
+                  About
+                </MenuItem>
+                <MenuItem as={MenuLink} href="/works">
+                  Works
+                </MenuItem>
+                <MenuItem as={MenuLink} href="https://store.craftz.dog/">
+                  Wallpapers
+                </MenuItem>
+                <MenuItem as={MenuLink} href="/posts">
+                  Posts
+                </MenuItem>
+                <MenuItem as={MenuLink} href="https://uses.craftz.dog/">
+                  Uses
+                </MenuItem>
                 <MenuItem
                   as={Link}
-                  href="https://github.com/faucetd/faucet-homepage"
+                  href="https://github.com/craftzdog/craftzdog-homepage"
                 >
                   View Source
                 </MenuItem>
